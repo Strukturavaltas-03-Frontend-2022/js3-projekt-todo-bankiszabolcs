f5ae17ea27aee163f875524054197dd45da9704f
szabi1112 <banki.szabolcs@gmail.com>
2022-09-05 02:05:01 +0200

    Nincs kész egészen :(

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
+import { getTodos, removeTodos, saveLocalToDos } from './localstorage.js';
+
 const input = document.querySelector('.input');
 const button = document.querySelector('button');
-const toDoList = document.querySelector('.toDoList');
+export const toDoList = document.querySelector('.toDoList');
 const pendingItems = document.querySelector('.pendingItems');
-const completedContainer = document.querySelector('.completedContainer');
+// const completedContainer = document.querySelector('.completedContainer');
+// const completedList = document.querySelector('.completedList');
 const day = document.querySelector('#day');
 const date = document.querySelector('#date');
 const numbCompletedItems = document.querySelector('.numbCompletedItems');
-const bottomContainer = document.querySelector('.bottomContainer');
+export const bottomContainer = document.querySelector('.bottomContainer');
 const hideComplete = document.querySelector('.hideComplete');
 const checkbox = document.querySelector('#checkit');
+const clearButton = document.querySelector('.clearAll');
 
 const now = new Date();
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 // Event Listeners
+window.addEventListener('DOMContentLoaded', getTodos)
 button.addEventListener('click', addToDo);
 toDoList.addEventListener('click', deleteItem);
 // toDoList.addEventListener('click', addToCompleted);
+clearButton.addEventListener('click', clearAll);
 hideComplete.addEventListener('click', () => {
   checkbox.checked === true ? filterToDo() : filterAll();
 });
@@ -25,17 +31,23 @@ checkbox.addEventListener('click', showOrHide);
 // Functions
 
 function setupDate() {
-  day.textContent = days[now.getDay()]
-  date.textContent = now.toLocaleDateString('en')
+  day.textContent = days[now.getDay()];
+  date.textContent = now.toLocaleDateString('en');
 }
 
 setupDate();
 
-function countPending() {
+export function countPending() {
   const number = Array.from(toDoList.children).filter((item) => !item.classList.contains('completed')).length;
-  console.log(Array.from(toDoList));
-  console.log(number);
   pendingItems.innerText = `You have ${number} pending items`;
+}
+
+export function counterCompleted() {
+  const completedNumb = Array.from(toDoList.children).filter((item) => item.classList.contains('completed')).length;
+  const all = Array.from(toDoList.children).length;
+  const sum = (completedNumb / all) * 100;
+  numbCompletedItems.innerText = `Completed tasks: ${sum}%`;
+  numbCompletedItems.style.display = 'block';
 }
 
 function addToDo(event) {
@@ -46,17 +58,18 @@ function addToDo(event) {
   const toDoDiv = document.createElement('div');
   toDoDiv.classList.add('toDoList__item');
 
-  // Make li
-  const toDoLi = document.createElement('li');
-  toDoLi.innerText = input.value;
-  // toDoLi.classList.add('toDoList__item');
-  toDoDiv.appendChild(toDoLi);
-
   // Checked Button
   const checkedMarkButton = document.createElement('button');
   checkedMarkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
   checkedMarkButton.classList.add('button--checked');
   toDoDiv.appendChild(checkedMarkButton);
+
+  // Make li
+  const toDoLi = document.createElement('li');
+  toDoLi.innerText = input.value;
+  // toDoLi.classList.add('toDoList__item');
+  toDoDiv.appendChild(toDoLi);
+  saveLocalToDos(input.value);
 
   // Trash Button
   const trashButton = document.createElement('button');
@@ -72,13 +85,18 @@ function addToDo(event) {
 
   bottomContainer.style.display = 'flex';
   countPending();
+  counterCompleted();
 }
 
-function addToCompleted(event) {
-  completedContainer.style.display = 'block';
+/* function addToCompleted(e) {
+  counterCompleted();
+  const actualItem = e.target;
+  completedContainer.style.display = 'flex';
+  completedList.style.display = 'block';
   // Make Div
   const completedDiv = document.createElement('div');
   completedDiv.classList.add('toDoList__item');
+  completedDiv.classList.add('completedDiv');
 
   // Make li
   const completedLi = document.createElement('li');
@@ -86,16 +104,19 @@ function addToCompleted(event) {
   completedDiv.appendChild(completedLi);
 
   // Add item to the list
-  completedContainer.appendChild(completedDiv);
+  completedList.appendChild(completedDiv);
 
-  pendingItems.innerText = `You have ${toDoList.childElementCount} pending items`;
+  countPending();
+
 }
-
+ */
 function deleteItem(e) {
   const actualItem = e.target;
   console.log(actualItem);
   if (actualItem.classList[0] === 'button--trash') {
     actualItem.parentElement.classList.add('fall');
+    countPending();
+    removeTodos(actualItem.parentElement)
     actualItem.parentElement.addEventListener('transitionend', () => {
       actualItem.parentElement.remove();
       pendingItems.innerText = `You have ${toDoList.childElementCount} pending items`;
@@ -103,13 +124,14 @@ function deleteItem(e) {
         document.querySelector('.noToDos').style.display = 'block';
         pendingItems.style.display = 'none';
         bottomContainer.style.display = 'none';
+        numbCompletedItems.style.display = 'none';
       }
     });
   }
-
   if (actualItem.classList[0] === 'button--checked') {
     actualItem.parentElement.classList.toggle('completed');
     countPending();
+    counterCompleted();
   }
 }
 
@@ -131,14 +153,22 @@ function filterAll(e) {
 
 function showOrHide() {
   const label = document.querySelector('#hideshow');
-  console.log(label);
-  console.log(checkbox);
+
   if (checkbox.checked === false) {
     label.textContent = 'Hide completed';
-    console.log('hasmi');
   } else {
     label.textContent = 'Show Completed';
-    console.log('kampo');
   }
   checkbox.checked === false ? true : '';
+}
+
+function clearAll() {
+  const toDos = toDoList.children;
+  console.log('fdfd');
+  Array.from(toDos).forEach((item) => item.remove());
+  document.querySelector('.noToDos').style.display = 'block';
+  pendingItems.style.display = 'none';
+  bottomContainer.style.display = 'none';
+  numbCompletedItems.style.display = 'none';
+  localStorage.clear()
 }
---
+++ localstorage.js
@@ -0,0 +1,62 @@
+import {
+  toDoList, bottomContainer, countPending, counterCompleted,
+} from '/app.js';
+
+function getLocalStorage() {
+  let todos;
+  if (localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+  return todos;
+}
+
+export function saveLocalToDos(todo) {
+  const todos = getLocalStorage();
+  todos.push(todo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+export function getTodos() {
+  const todos = getLocalStorage();
+  todos.forEach((todo) => {
+    document.querySelector('.noToDos').style.display = 'none';
+    // Make Div
+    const toDoDiv = document.createElement('div');
+    toDoDiv.classList.add('toDoList__item');
+
+    // Checked Button
+    const checkedMarkButton = document.createElement('button');
+    checkedMarkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    checkedMarkButton.classList.add('button--checked');
+    toDoDiv.appendChild(checkedMarkButton);
+
+    // Make li
+    const toDoLi = document.createElement('li');
+    toDoLi.innerText = todo;
+    // toDoLi.classList.add('toDoList__item');
+    toDoDiv.appendChild(toDoLi);
+
+    // Trash Button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    trashButton.classList.add('button--trash');
+    toDoDiv.appendChild(trashButton);
+
+    // Add item to the list
+    toDoList.appendChild(toDoDiv);
+
+    bottomContainer.style.display = 'flex';
+    countPending();
+    counterCompleted();
+  });
+}
+
+export function removeTodos(todo) {
+  const todos = getLocalStorage();
+  const toDoIndex = todo.children[1].innerText;
+  console.log(todos);
+  todos.splice(todos.indexOf(toDoIndex), 1);
+  localStorage.setItem('todos', JSON.stringify(todos))
+}
